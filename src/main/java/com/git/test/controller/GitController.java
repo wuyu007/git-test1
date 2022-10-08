e0beb70497e42e79288e855d1bf75fff2b6f3a27
itcast <2067378648@qq.com>
2022-10-08 17:56:39 +0800

    first commit

--- src/main/java/com/git/test/controller/GitController.java
+++ src/main/java/com/git/test/controller/GitController.java
@@ -1,4 +1,21 @@
 package com.git.test.controller;
 
+
 public class GitController {
+
+    public void add(){
+        System.out.println("add...");
+    }
+
+    public void delete(){
+        System.out.println("delete...");
+    }
+
+    public void update(){
+        System.out.println("update...");
+    }
+
+    public void find(){
+        System.out.println("find...");
+    }
 }
